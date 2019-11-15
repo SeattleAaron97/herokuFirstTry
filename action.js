a5555df7fb871619fbec856f4e228a11d54da387
SeattleAaron97 <aaron.m.schechter@gmail.com>
2019-11-15 10:15:41 -0800

    Add files via upload

---
+++ action.js
@@ -0,0 +1,6 @@
+//js file to do my things in!!
+
+function testMethod(){
+	
+		console.log("in the test method!!");
+}
--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+	<script src="action.js"></script>
+	<meta charset="UTF-8">
     <title>Quick Heroku Deployment test</title>
+	
   </head>
   <body>
 	
 	<h1>Heeeeloooooooo world!!!</h1>
+	<br>
+	<h3>This line might show up!</h3>
+	<button onclick="testMethod"> Click me! </button>
 	
   </body>
 </html>
