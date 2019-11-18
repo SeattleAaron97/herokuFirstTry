a715d3560f4ec5e9f07aa818ba2fe3c473dd434e
SeattleAaron97 <aaron.m.schechter@gmail.com>
2019-11-18 14:49:34 -0800

    Add files via upload

--- action.js
+++ action.js
@@ -1,6 +1,64 @@
 //js file to do my things in!!
+var counter = 0;
+var locationCounter = 0;
 
-function testMethod(){
+	var myTable= "<table><tr><td style='width: 100px; color: red;'>Entry Number</td>";
+	myTable+= "<td style='width: 100px; color: red; text-align: right;'>Sex</td>";
+	myTable+="<td style='width: 100px; color: red; text-align: right;'>Mean</td></tr>";
+
+	myTable+="<tr><td style='width: 100px;                   '>---------------</td>";
+	myTable+="<td     style='width: 100px; text-align: right;'>---------------</td>";
+	myTable+="<td     style='width: 100px; text-align: right;'>---------------</td></tr>";
+
+function dataDump(){
+		locationCounter = 0;
+        d3.csv("GBD_2017_death_rate_opioid_use_disorders_all_ages.csv", function (data){
+
+			showDataForCountryAndYear(data);
+			
+		myTable+="</table>";	
+		document.getElementById('tablePrint').innerHTML = myTable;
+			
+
+            ///$("#csvData").text(JSON.stringify(data))
+        })
+}	
+
+
+function countCountry(data){
+	var selectedLocation = document.getElementById("locationId").value;
+	console.log('selectedLocation = '+ selectedLocation);
+	if(data.location == selectedLocation){
+		locationCounter ++;
+	}
+		
+	console.log('locationCounter = '+ locationCounter);
 	
-		console.log("in the test method!!");
+}
+
+function showDataForCountryAndYear(data){
+	var currentMean = 0;
+	var selectedLocation = document.getElementById("locationId").value;
+	var selectedYear = document.getElementById("yearId").value;
+	var myArray    = new Array();
+	
+
+	if((data.location == selectedLocation) && (data.year = selectedYear)){
+		currentMean = data.mean;
+		currentSex = data.sex;
+		locationCounter ++;
+		myTable+="<tr><td style='width: 100px;'>Number " + locationCounter + " is: 	&nbsp&nbsp&nbsp&nbsp </td>";
+		myTable+="<td style='width: 100px; text-align: right;'>" +" " + currentSex + " " + "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</td>";
+		myTable+="<td style='width: 100px; text-align: right;'>" + currentMean + "</td><br>";
+
+	}	
+
+}
+
+
+function printCountries(data){
+	counter ++;
+	var currentCountry = data.location;
+	console.log('currentCountry = '+ currentCountry);
+	console.log(counter);
 }
--- index.php
+++ index.php
@@ -2,17 +2,31 @@
 <html>
   <head>
     <link rel="stylesheet" type="text/css" href="styles.css">
-	<script src="action.js"></script>
 	<meta charset="UTF-8">
     <title>Quick Heroku Deployment test</title>
-	
+	<script src="https://d3js.org/d3.v5.min.js"></script>
   </head>
   <body>
-	
-	<h1>Heeeeloooooooo world!!!</h1>
+	<script src="action.js"></script>
+	<h1>Hello, welcome all viewers!</h1>
 	<br>
-	<h3>This line might show up!</h3>
-	<button onclick="testMethod"> Click me! </button>
+	<p> This page is a data visualization for the UW IMHE coding challenge. For reference, the three numeric data points are </p>
+	
+	<p>Mean: This is the mean death rate (number of deaths per 100,000 people) due to opioid use disorders for a given location, sex, and year.</p>
+	<p>Upper: This is the upper bound of uncertainty for the mean</p>
+	<p>Lower: This is the lower bound of uncertainty for the mean</p>
+
+
+
+	Country Name:<br>
+	<input type="text" name="location" id="locationId"><br>
+	Year (1990 - 2017):<br>
+	<input type="number" name="yaer" id="yearId"><br>
+	<button onclick="dataDump()"> Click me! </button>
+	
+	<p>after clicking the button, please wait about 20 seconds, the action is slow.</p>
+    <div id="tablePrint"> </div>
+
 	
   </body>
 </html>
